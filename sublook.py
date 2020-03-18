69c62237497fea896a1537eb81ce267664604a36
mehmetserifpasa <46201251+mehmetserifpasa@users.noreply.github.com>
2020-03-18 23:26:01 +0300

    /

---
+++ sublook.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+__author__ = 'mehmet şerif paşa'
+
+import random
+import socket
+import sys
+import threading
+import time
+import colorama
+import requests
+
+USERAGENT = [agent.strip() for agent in open('useragent.txt')]
+SUBDOMAIN = [sub.strip() for sub in open('sublist.txt')]
+
+zaman = time.time()
+
+
+def Saved(put):
+    file = open("output.txt","a+")
+    file.writelines(str(put)+"\n")
+
+
+say = 0
+def Subdomain_Status_Look():
+    global say
+    try:
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect((SUBDOMAIN[say], 80))
+        sock.send("GET / HTTP/1.1\r\nHost: %s\r\n\r\n" % SUBDOMAIN[say])
+        response = sock.recv(4096)
+        print(colorama.Fore.GREEN + SUBDOMAIN[say]+ colorama.Style.RESET_ALL)
+        print(response)
+        print("-"*60)
+        if int(response.split()[1]) == 200:
+            Saved(str(SUBDOMAIN[say]) + " - " + str(response.split()[1]))
+        """
+                if int(response.split()[1]) == 301:
+            Saved(str(SUBDOMAIN[say]) + " - " + str(response.split()[1]))
+        """
+        # Hangi STATUS KODLARI(200,301,404 vb.) İstiyorsanız ayarlayın.
+    except:
+        print("Connection Error ==> "+ SUBDOMAIN[say])
+    say +=1
+
+
+
+thread = []
+for i in range(len(SUBDOMAIN)):
+    th = threading.Thread(target=Subdomain_Status_Look)
+    thread.append(th)
+    th.start()
+    time.sleep(0.2)
+
+for thjoin in thread:
+    thjoin.join()
+
+Son_zaman = int(time.time() - int(zaman))
+print("\n\nGeçen Zaman: "+ str(Son_zaman))
